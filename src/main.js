ab22c23b56aac22963106beb3d39b6172f2672c7
MondaSamuel <samwelmonda48@gmail.com>
2025-03-01 20:59:07 +0300

    added an about pageiuuuuuuuuiuuuuuiiwds

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag';
 
 const app = createApp(App)
+
+app.use(VueGtag, {
+    config: { id: 'G-9S98390Y5M' }, // Replace with your GA Measurement ID
+  }, router);
+  
 
 app.use(createPinia())
 app.use(router)
